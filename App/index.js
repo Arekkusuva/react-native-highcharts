90a823d500b28b22e160a5f32301db5dbafa860f
Alexey <vuraddo@yahoo.com>
2017-11-01 15:11:48 +0700

    added webViewProps

--- App/index.js
+++ App/index.js
@@ -95,6 +95,7 @@ class ChartWeb extends Component {
           scalesPageToFit={true}
           scrollEnabled={false}
           automaticallyAdjustContentInsets={true}
+          {...this.props.webViewProps}
         />
       </View>
     );
